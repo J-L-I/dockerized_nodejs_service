5476028acfd3751bd92497509f7797db47e5ee22
J-L-I <jerry.inm07@gmail.com>
2025-10-07 15:52:10 -0600

    app.js

---
+++ app.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require('express');
+const basicAuth = require('express-basic-auth');
+const app = express();
+const port = process.env.PORT;
+
+
+app.use('/secret', basicAuth({
+    users: { [process.env.USERNAME]: process.env.USER_PW},
+    challenge: true,
+    unauthorizedResponse: () => "Incorrect Credentials!"
+}));
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/secret', (req, res) => {
+    res.send(process.env.SECRET_MESSAGE)
+})
+
+app.listen(port, () => {
+    console.log(`App is listening on port ${port}`)
+})
